54fea1f457b3684526f3188618e8f41851006d6f
dh7016 <dh7016w@hotmail.com>
2014-12-15 11:37:40 -0500

    0.2.7
    0.2.7

--- game-server/app.js
+++ game-server/app.js
@@ -13,10 +13,10 @@ app.configure('production|development', 'connector', function(){
       connector : pomelo.connectors.sioconnector,
       //websocket, htmlfile, xhr-polling, jsonp-polling, flashsocket
       transports : ['websocket'],
-      heartbeats : true,
+      heartbeats : 3,
       closeTimeout : 60,
-      heartbeatTimeout : 60,
-      heartbeatInterval : 20
+      //heartbeatTimeout : 60,
+      //heartbeatInterval : 20
     });
 });
 

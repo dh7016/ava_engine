1181cb8a356bcd8e94b2f4865bb4704ce458e1e5
dh7016 <dh7016w@hotmail.com>
2014-12-24 01:44:04 -0500

    0.4.9
    0.4.9

--- game-server/util/dispatcher.js
+++ game-server/util/dispatcher.js
@@ -4,6 +4,6 @@ var crc = require('crc');
 
 //暂时随机算法
 module.exports.dispatch = function(connectors) {
-	var index = int(Math.random()*connectors.length);
+	//var index = int(Math.random()*connectors.length);
 	return connectors[0];
 };

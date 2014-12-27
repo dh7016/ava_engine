b82b8b9c5b8bb2ddb8ed3b3f1f560569f0d7d713
dh7016 <dh7016w@hotmail.com>
2014-12-27 10:01:45 -0500

    0.5.4
    0.5.4

--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -202,9 +202,9 @@ Handler.prototype.subscribe = function(msg, session, next) {
 Handler.prototype.loadConfig = function (msg, session, next) {
 
 	//得到配置文件
-    var gameConfig={config:"cwnlcijjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"};
+    var gameConfig="cwnlcijjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"};
 
     //返回配置文件
-    next(null,gameConfig);
+    next(null,{code:100,config:gameConfig});
 
 }

0b0892dc5050a28a3889cd3c0aa722b8b476e57f
dh7016 <dh7016w@hotmail.com>
2014-12-28 03:57:13 -0500

    0.5.0
    0.5.0

--- game-server/app/database/userDb.js
+++ game-server/app/database/userDb.js
@@ -87,7 +87,8 @@ userDb.registerByDid = function (did, cb) {
   				args = [did];
 
   				 pomelo.app.get('dbclient').query(sql,args,function(err, res) {
-  				 	
+  				 		console.log(res);
+  				 	console.log(res.length);
 
   				 	if(err !== null) {
   				 		cb(null,false);

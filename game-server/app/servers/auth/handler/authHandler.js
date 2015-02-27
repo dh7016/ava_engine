5de6a8ef6127163c143dffa409fd8c03d0175cc7
dh7016 <dh7016w@hotmail.com>
2015-02-27 04:47:03 -0500

    0.5.0
    0.5.0

--- game-server/app/servers/auth/handler/authHandler.js
+++ game-server/app/servers/auth/handler/authHandler.js
@@ -32,7 +32,7 @@ Handler.prototype.registerByUsername = function(msg,session,next) {
 //验证 用户信息
 Handler.prototype.AuthByUsername = function(msg,session,next) {
 
-	var username=msg.username;
+	/*var username=msg.username;
 	var password=msg.password;
 
 
@@ -48,7 +48,8 @@ Handler.prototype.AuthByUsername = function(msg,session,next) {
 			next(null,{signal:0});
 		}
 	}	
-	)
+	)*/
+	console.log(12345);
 }
 
 

74bd94b0ba48cbf34d1756f127677307ecfbb4d1
dh7016 <dh7016w@hotmail.com>
2015-03-10 00:56:59 -0400

    0.5.0

--- game-server/app/acc/supportFunction.js
+++ game-server/app/acc/supportFunction.js
@@ -4,7 +4,12 @@ var supportFunction = module.exports;
 
 //string转化为json
 supportFunction.stringToJson = function(_str) {
-	return JSON.parse(_str);
+	if(_str===""){
+		return "";
+	}
+	else {
+		return eval("(" + _str + ")");
+	}
 }
  //json转化为string
  supportFunction.jsonToString = function(_json) {
